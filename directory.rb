dad86773fde6c214b0af21c7891fbc33b95ae398
mat <matmoon14@gmail.com>
2019-03-01 20:41:03 +0000

    more

--- directory.rb
+++ directory.rb
@@ -13,11 +13,20 @@ students = [
   "Norman Bates"
 ]
 
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-    puts student
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
 end
 
-
-puts "Overall, we have #{student_count} great students"
+print_header
+print(students)
+print_footer(students)
+	
