9a13069032f096ea33c46ef69dfefca525a36b09
mat <matmoon14@gmail.com>
2019-03-02 19:51:11 +0000

    more

--- directory.rb
+++ directory.rb
@@ -1,34 +1,60 @@
+@students = []
 
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  name = gets.chomp
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    name = gets.chomp
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit 
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  students = []
-  name = gets.chomp
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
-  end
-  students
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
